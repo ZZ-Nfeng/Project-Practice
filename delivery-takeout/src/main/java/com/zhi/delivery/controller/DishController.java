972a5430d59bdc538b0a2f880778eceb0f4764d2
Nfeng <1852149514@qq.com>
2023-06-12 14:43:20 +0800

    修改提交

--- delivery-takeout/src/main/java/com/zhi/delivery/controller/DishController.java
+++ delivery-takeout/src/main/java/com/zhi/delivery/controller/DishController.java
@@ -1,6 +1,8 @@
 package com.zhi.delivery.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zhi.delivery.common.Result;
 import com.zhi.delivery.dto.DishDto;
@@ -14,11 +16,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/dish")
@@ -39,24 +44,81 @@ public class DishController {
 
     @PostMapping
     public Result<String> save(@RequestBody DishDto dishDto){
+        List<DishFlavor> dishFlavors = dishDto.getFlavors();
+        dishFlavors.stream().map(dishFlavor -> {
+            dishFlavor.setDishId(dishDto.getId());
+            return dishFlavor;
+        }).collect(Collectors.toList());
+        dishFlavorService.saveBatch(dishFlavors);
+
+        Dish dish = new Dish();
+        BeanUtil.copyProperties(dishDto,dish);
+        dishService.save(dish);
+
+        return Result.success("添加成功");
 
     }
 
 
     @GetMapping("/page")
     public Result<Page> pageShow(int page,int pageSize,String name){
+        Page<Dish> dishPage = new Page<>(page, pageSize);
+        Page<DishDto> dishDtoPage = new Page<>();
 
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(name!=null,Dish::getName,name);
+        dishService.page(dishPage);
+
+        List<Dish> dishList = dishPage.getRecords();
+
+        List<DishDto> collect = dishList.stream().map(dish -> {
+            DishDto dishDto = new DishDto();
+            BeanUtil.copyProperties(dish, dishDto);
+
+            LambdaQueryWrapper<Category> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+            lambdaQueryWrapper.eq(Category::getId, dish.getCategoryId());
+            Category category = categoryService.getOne(lambdaQueryWrapper);
+            dishDto.setCategoryName(category.getName());
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        BeanUtil.copyProperties(dishPage,dishDtoPage);
+        dishDtoPage.setRecords(collect);
+        return Result.success(dishDtoPage);
     }
 
     @GetMapping("/{id}")
     public Result<DishDto> get(@PathVariable Long id){
+        Dish dish = dishService.getById(id);
+        DishDto dishDto = new DishDto();
+        BeanUtil.copyProperties(dish,dishDto);
 
-
+        LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(DishFlavor::getDishId,id);
+        List<DishFlavor> list = dishFlavorService.list(queryWrapper);
+        dishDto.setFlavors(list);
+        return Result.success(dishDto);
     }
 
+    @Transactional
     @PutMapping
     public Result<String> update(@RequestBody DishDto dishDto){
+        //更新两张表
+        Dish dish = new Dish();
+        BeanUtil.copyProperties(dishDto,dish);
+        dishService.saveOrUpdate(dish);
+        //先删除原来的数据
 
+        Long dishId = dishDto.getId();
+        dishFlavorService.remove(new LambdaUpdateWrapper<DishFlavor>().eq(DishFlavor::getDishId,dishId));
+
+        List<DishFlavor> flavors = dishDto.getFlavors();
+        flavors.stream().map(dishFlavor -> {
+            dishFlavor.setDishId(dishDto.getId());
+            return dishFlavor;
+        }).collect(Collectors.toList());
+        dishFlavorService.saveBatch(flavors);
+        return Result.success("删除成功");
     }
 
 
@@ -65,18 +127,39 @@ public class DishController {
     // 根据条件(分类id)查询对应的菜品数据
     @GetMapping("/list")
     public Result<List<DishDto>> list(Dish dish){
+        Long categoryId = dish.getCategoryId();
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Dish::getCategoryId,categoryId);
+        Dish one = dishService.getOne(queryWrapper);
+
+        List<DishFlavor> dishFlavorList = dishFlavorService.list(new LambdaUpdateWrapper<DishFlavor>().eq(DishFlavor::getDishId, one.getId()));
+
+        return null;
 
     }
 
     // 改变菜品的销售状态
     @PostMapping("/status/{status}")
     public Result<String> updateSaleStatus(@PathVariable("status") Integer status,@RequestParam List<Long> ids){
-
-
+        List<Dish> dishList = dishService.listByIds(ids);
+        dishList.stream().map(dish -> {
+            dish.setStatus(status);
+            dishService.updateById(dish);
+            return dish;
+        }).collect(Collectors.toList());
+        return Result.success("更改成功");
     }
 
     @DeleteMapping
     public Result<String> batchDelete(@RequestParam("ids") List<Long> ids){
+        dishService.removeByIds(ids);
+        //查出所有的口味表
+        for (Long id : ids) {
+            LambdaQueryWrapper<DishFlavor> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(DishFlavor::getDishId,id);
+            dishFlavorService.remove(queryWrapper);
+        }
+        return Result.success("删除成功");
 
     }
 
