a703b2196629c4fd58791be237968f671044f6f7
Nfeng <1852149514@qq.com>
2023-06-13 20:50:26 +0800

    修改提交

---
+++ delivery-takeout/src/main/java/com/zhi/delivery/controller/OrderController.java
@@ -0,0 +1,190 @@
+package com.zhi.delivery.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import com.zhi.delivery.common.BaseContext;
+import com.zhi.delivery.common.MyCustomException;
+import com.zhi.delivery.common.Result;
+import com.zhi.delivery.dto.OrdersDto;
+import com.zhi.delivery.entity.*;
+import com.zhi.delivery.service.*;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/order")
+@Slf4j
+public class OrderController {
+
+    @Autowired
+    private AddressBookService addressBookService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+    @Autowired
+    private OrdersService ordersService;
+
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    @PostMapping("/submit")
+    public Result<String> submit(@RequestBody Orders orders) {
+
+        log.info("订单信息:" + orders.toString());
+
+
+        // 获取当前用户的id
+        Long userId = BaseContext.getCurrentId();
+
+        // 查询当前用户的 购物车数据
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+
+        queryWrapper.eq(ShoppingCart::getUserId,userId);
+
+        // 一个用户可能下单了 多个菜品或套餐，故应该查询到的是一个 购物车类型的list
+        List<ShoppingCart> cartList = shoppingCartService.list(queryWrapper);
+
+        if (cartList == null || cartList.size() == 0){
+            throw new MyCustomException("购物车为空，不能下单！");
+        }
+
+        // 查询用户数据
+        User user = userService.getById(userId);
+
+        // 查询用户的派送地址信息
+        AddressBook addressBook = addressBookService.getById(orders.getAddressBookId());
+        if (addressBook == null){
+            throw new MyCustomException("您的地址信息有误，暂不能下单!");
+        }
+
+
+        long orderId = IdWorker.getId();  // 订单号
+
+        //  购物车中 商品 的总金额 需要保证在多线程的情况下 也是能计算正确的，故需要使用原子类
+        AtomicInteger amount = new AtomicInteger(0);
+
+        List<OrderDetail> orderDetails = cartList.stream().map((item) -> {
+            OrderDetail orderDetail = new OrderDetail();
+
+            orderDetail.setOrderId(orderId);
+            orderDetail.setName(item.getName());
+            orderDetail.setImage(item.getImage());
+
+            orderDetail.setDishId(item.getDishId());
+            orderDetail.setSetmealId(item.getSetmealId());
+            orderDetail.setDishFlavor(item.getDishFlavor());
+            orderDetail.setNumber(item.getNumber());
+            orderDetail.setAmount(item.getAmount());
+
+            amount.addAndGet(item.getAmount().multiply(new BigDecimal(item.getNumber())).intValue());
+
+            return orderDetail;
+
+        }).collect(Collectors.toList());
+
+        orders.setId(orderId);
+        orders.setOrderTime(LocalDateTime.now());
+        orders.setCheckoutTime(LocalDateTime.now());
+        orders.setStatus(2);
+        orders.setAmount(new BigDecimal(amount.get()));//总金额，需要 遍历购物车，计算相关金额来得到
+        orders.setUserId(userId);
+        orders.setNumber(String.valueOf(orderId));
+        orders.setUserName(user.getName());
+        orders.setConsignee(addressBook.getConsignee());
+        orders.setPhone(addressBook.getPhone());
+        orders.setAddress((addressBook.getProvinceName() == null ? "" : addressBook.getProvinceName())
+                + (addressBook.getCityName() == null ? "" : addressBook.getCityName())
+                + (addressBook.getDistrictName() == null ? "" : addressBook.getDistrictName())
+                + (addressBook.getDetail() == null ? "" : addressBook.getDetail()));
+
+
+        // 向订单表插入数据,一条数据，插入数据之前，需要填充如上属性
+        ordersService.save(orders);    //  --> ordersService.save(orders);
+
+        // 向订单明细表插入数据，多条数据
+        orderDetailService.saveBatch(orderDetails);
+
+        // 清空购物车数据
+        shoppingCartService.remove(queryWrapper);
+
+
+
+        return Result.success("已成功下单!");
+    }
+
+
+    //    http://localhost:8181/order/page?page=1&pageSize=10&number=11
+    @GetMapping("/page")
+    public Result<Page> showPage(int page, int pageSize, Long number,String beginTime,String endTime) {
+
+        Page<Orders> ordersPage = new Page(page, pageSize);
+
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(number != null, Orders::getNumber, number)
+                .gt(StringUtils.isNotEmpty(beginTime),Orders::getOrderTime,beginTime)
+                .lt(StringUtils.isNotEmpty(endTime),Orders::getOrderTime,endTime);
+
+        ordersService.page(ordersPage, queryWrapper);
+        return Result.success(ordersPage);
+    }
+
+    // http://localhost:8181/order/userPage?page=1&pageSize=5
+    @GetMapping("/userPage")
+    public Result<Page> page(int page, int pageSize){
+        //分页构造器对象
+        Page<Orders> pageInfo = new Page<>(page,pageSize);
+        Page<OrdersDto> pageDto = new Page<>(page,pageSize);
+        //构造条件查询对象
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        //这里树直接把分页的全部结果查询出来，没有分页条件
+        //添加排序条件，根据更新时间降序排列
+        queryWrapper.orderByDesc(Orders::getOrderTime);
+        this.ordersService.page(pageInfo,queryWrapper);
+
+        //通过OrderId查询对应的OrderDetail
+        LambdaQueryWrapper<OrderDetail> queryWrapper2 = new LambdaQueryWrapper<>();
+
+        //对OrderDto进行需要的属性赋值
+        List<Orders> records = pageInfo.getRecords();
+        List<OrdersDto> orderDtoList = records.stream().map((item) ->{
+            OrdersDto orderDto = new OrdersDto();
+            //此时的orderDto对象里面orderDetails属性还是空 下面准备为它赋值
+            Long orderId = item.getId();//获取订单id
+            List<OrderDetail> orderDetailList = getOrderDetailsByOrderId(orderId);
+            BeanUtils.copyProperties(item,orderDto);
+            //对orderDto进行OrderDetails属性的赋值
+            orderDto.setOrderDetails(orderDetailList);
+            return orderDto;
+        }).collect(Collectors.toList());
+
+        //使用dto的分页有点难度.....需要重点掌握
+        BeanUtils.copyProperties(pageInfo,pageDto,"records");
+        pageDto.setRecords(orderDtoList);
+        return Result.success(pageDto);
+    }
+
+    public List<OrderDetail> getOrderDetailsByOrderId(Long orderId){
+        LambdaQueryWrapper<OrderDetail> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(OrderDetail::getOrderId, orderId);
+        List<OrderDetail> orderDetailList = orderDetailService.list(queryWrapper);
+        return orderDetailList;
+    }
+
+    @PutMapping
+    public Result<String> updateStatus(Orders orders){
+        ordersService.updateById(orders);
+        return Result.success("修改成功");
+    }
+}
--- delivery-takeout/src/main/java/com/zhi/delivery/controller/ShoppingCartController.java
+++ delivery-takeout/src/main/java/com/zhi/delivery/controller/ShoppingCartController.java
@@ -23,39 +23,30 @@ public class ShoppingCartController {
     public Result<ShoppingCart> addToCart(@RequestBody ShoppingCart shoppingCart){
         log.info("购物车中的数据:{}"+shoppingCart.toString());
         
-        //设置用户id,指定当前是哪个用户的 购物车数据
-        Long userId = BaseContext.getCurrentId();
-        shoppingCart.setUserId(userId);
-        
-        // 查询当前菜品或套餐是否 在购物车中
-        Long dishId = shoppingCart.getDishId();
+       //获取当前用户
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
 
         LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(ShoppingCart::getUserId,userId);  // 根据登录用户的 userId去ShoppingCart表中查询该用户的购物车数据
-
-        if (dishId != null){ // 添加进购物车的是菜品，且 购物车中已经添加过 该菜品
-             queryWrapper.eq(ShoppingCart::getDishId,dishId);
+        queryWrapper.eq(ShoppingCart::getUserId,currentId);
+        //判断当前菜品在没在数据库里，在数量加一，不在添加进去
+        if(shoppingCart.getDishId()!=null){
+            queryWrapper.eq(ShoppingCart::getDishId,shoppingCart.getDishId());
         }else {
-            queryWrapper.eq(ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
+            queryWrapper.eq(shoppingCart.getSetmealId()!=null,ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
         }
-
-
-        ShoppingCart oneCart = shoppingCartService.getOne(queryWrapper);
-        //  如果购物车中 已经存在该菜品或套餐，其数量+1，不存在，就将该购物车数据保存到数据库中
-        if (oneCart != null){
-            Integer number = oneCart.getNumber();
-            oneCart.setNumber(number + 1);
-
-            shoppingCartService.updateById(oneCart);
-        }else {
-            shoppingCart.setNumber(1);
-            shoppingCartService.save(shoppingCart);
-
-            oneCart = shoppingCart;
+        ShoppingCart shoppingCartServiceOne = shoppingCartService.getOne(queryWrapper);
+        //存在，数量加1
+        if(shoppingCartServiceOne!=null){
+            Integer number = shoppingCartServiceOne.getNumber();
+            shoppingCartServiceOne.setNumber(number+1);
+            shoppingCartService.updateById(shoppingCart);
+            return Result.success(shoppingCartServiceOne);
         }
-
-
-        return Result.success(oneCart);
+        //不存在,保存进数据库
+        shoppingCart.setNumber(1);
+        shoppingCartService.save(shoppingCart);
+        return Result.success(shoppingCart);
     }
 
     // 在购物车中删减订单
@@ -79,7 +70,7 @@ public class ShoppingCartController {
 
 
         ShoppingCart oneCart = shoppingCartService.getOne(queryWrapper);
-        //  如果购物车中 已经存在该菜品或套餐，其数量+1，不存在，就将该购物车数据保存到数据库中
+
         if (oneCart != null){
             Integer number = oneCart.getNumber();
             if (number != 0){
@@ -98,7 +89,7 @@ public class ShoppingCartController {
 
         LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(ShoppingCart::getUserId,BaseContext.getCurrentId());
-
+        queryWrapper.gt(ShoppingCart::getNumber,0);
         // 最晚下单的 菜品或套餐在购物车中最先展示
         queryWrapper.orderByDesc(ShoppingCart::getCreateTime);
         List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
